fbb85ba35ee9e25197c3ff0e76baf4d4bae76bfe
Baris Atak <39070206+BarisATAK@users.noreply.github.com>
2026-02-05 12:28:48 +0300

    Update visa_bot.py

--- visa_bot.py
+++ visa_bot.py
@@ -60,7 +60,16 @@ def is_open_by_keywords(page_text, extra_closed=None):
 BLS_URL = "https://turkey.blsspainglobal.com/Global/Appointment/NewAppointment"
 def is_bls_es_open():
     try:
-        r = requests.get(BLS_URL, timeout=20)
+        headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                      "AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/121.0.0.0 Safari/537.36",
+        "Accept-Language": "tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Connection": "keep-alive"
+    }
+
+        r = requests.get(BLS_URL, headers=headers, timeout=20)
         print(r.text)#debug.
         return is_open_by_keywords(r.text)
     except Exception as e:
